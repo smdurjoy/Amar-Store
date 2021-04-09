617f7ef13bda2ada7d6dd9d0ef550c5b8cdccca3
smdurjoy <smdurjoy963@gmail.com>
2021-04-09 17:18:30 +0600

    Order status update with sms and email

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -8,6 +8,8 @@ use App\OrderStatus;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use App\Sms;
+use Illuminate\Support\Facades\Mail;
 
 class OrdersController extends Controller
 {
@@ -27,6 +29,31 @@ class OrdersController extends Controller
     function updateOrderStatus(Request $request) {
         Order::where('id', $request->order_id)->update(['order_status' => $request->order_status]);
         Session::flash('successMessage', 'Order status has been updated successfully.');
+
+        $deliveryDetails = Order::select('mobile', 'email', 'name')->where('id', $request->order_id)->first();
+
+        // Send order status update sms
+        $message = "Dear customer, Your order #".$request->order_id." status has been updated to '".$request->order_status."' placed with ecom.smdurjoy.com.";
+        $mobile = $deliveryDetails->mobile;
+        Sms::sendSms($message, $mobile);
+
+        // Send order status update email
+        $orderDetails = Order::where('id', $request->order_id)->with('order_products')->first();
+
+        // Send order email
+        $email = $deliveryDetails->email;
+        $messageData = [
+            'email' => $email,
+            'name' => $deliveryDetails->name,
+            'order_id' => $request->order_id,
+            'order_status' => $request->order_status,
+            'orderDetails' => $orderDetails,
+        ];  
+
+        Mail::send('emails.order_status', $messageData, function ($message) use ($email) {
+            $message->to($email)->subject('Order Status Updated - ecom.smdurjoy.com');
+        });
+
         return redirect()->back();
     }
-}
+}   
